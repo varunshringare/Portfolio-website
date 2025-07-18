2d98c738019288a95b42bb1793748185be8c0291
varun shringare <103264517+varunshringare@users.noreply.github.com>
2025-07-18 16:24:07 +0530

    Update Techno.jsx

--- src/components/Techno.jsx
+++ src/components/Techno.jsx
@@ -31,7 +31,7 @@ const Techno = ({ sectionId }) => {
         transition={{ duration: 0.5 }}
         className="my-20 text-center text-4xl"
       >
-        Technologies I've worked with
+        Technologies and platforms I've worked with
       </motion.h1>
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
